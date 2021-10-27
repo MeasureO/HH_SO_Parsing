33913e77d13e0f637489a43cc8d821f3873fafe8
MeasureO <42300157+MeasureO@users.noreply.github.com>
2021-10-27 05:03:14 +0300

    Add files via upload

---
+++ headhunter.py
@@ -0,0 +1,47 @@
+import requests
+from requests.api import head
+from bs4 import BeautifulSoup
+ITEMS = 100
+URL = f'https://hh.ru/search/vacancy?st=searchVacancy&text=python&items_on_page={ITEMS}'
+headers = {
+        'Host': 'hh.ru',
+        'User-Agent': 'Safari', 
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive'
+    }
+def extract_max_page():
+    hh_request = requests.get(URL, headers=headers)
+    hh_soup = BeautifulSoup(hh_request.text, 'html.parser')
+    paginator = hh_soup.find_all("span", {'class': 'pager-item-not-in-short-range'})
+    pages = []
+    for page in paginator:
+        pages.append(int(page.find('a').text))
+    return pages[-1]
+# for page in range(max_page):
+#     print(f'page={page}')
+def extract_job(html):
+    title = html.find("a").text.strip()
+    link = html.find("a")['href']
+    company = html.find("div", {"class": 'vacancy-serp-item__meta-info-company'}).text.strip().replace('\xa0', ' ')
+    location = html.find("div", {"data-qa": 'vacancy-serp__vacancy-address'}).text.strip()
+    location = location.partition(',')[0]
+    return {'title': title, 'company': company, 'location': location, 'link': link}
+
+def extract_jobs(last_page):
+    jobs = []
+    for page in range(last_page):
+        print(f"HeadHunter: parsing page={page}")
+        result = requests.get(f'{URL}&page={page}', headers=headers)
+        # print(result.status_code)
+        soup = BeautifulSoup(result.text, 'html.parser') 
+        results = soup.find_all("div", {'class': 'vacancy-serp-item'})
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
+    return jobs
+
+def get_jobs():
+    max_page = extract_max_page()
+    jobs = extract_jobs(max_page)
+    return jobs
---
+++ so.py
@@ -0,0 +1,47 @@
+import requests
+from requests.api import head
+from bs4 import BeautifulSoup
+
+URL = f'https://stackoverflow.com/jobs?q=python'
+headers = {
+        'Host': 'hh.ru',
+        'User-Agent': 'Safari', 
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive'
+    }
+
+def extract_max_page():
+    request = requests.get(URL)
+    soup = BeautifulSoup(request.text, 'html.parser')
+    pages = soup.find('div', {'class': 's-pagination'}).find_all('a')
+    last_page = int(pages[-2].get_text(strip=True))
+    return last_page
+
+def extract_job(html):
+    title = html.find('h2').find('a').text
+    company_row = html.find('h3').find_all('span', recursive=False)
+    company = company_row[0].get_text(strip=True)
+    location = company_row[1].get_text(strip=True)
+    job_id = html['data-jobid']
+    link = f'https://stackoverflow.com/jobs/{job_id}/'
+    return {'title': title, 'company': company, 'location': location, 'link': link}
+     
+def extract_jobs(last_page):
+    jobs = []
+    for page in range(last_page):
+        print(f"StackOverflow: parsing page={page + 1}")
+        result = requests.get(f'{URL}&pg={page + 1}')
+        soup = BeautifulSoup(result.text, 'html.parser')
+        results = soup.find_all('div', {'class': '-job'})
+        for result in results:
+            # print(result['data-jobid'])
+            job = extract_job(result)
+            jobs.append(job)
+    return jobs
+
+def get_jobs():
+    max_page = extract_max_page()
+    jobs = extract_jobs(max_page)
+    return jobs
+
